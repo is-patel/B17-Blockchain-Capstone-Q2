bae8da4711d2fb19fad7cbd563ddd16544a26185
DhruvK0 <kanetkard@gmail.com>
2024-12-04 09:39:35 -0800

    added more cities to zillow scrape and the csv output

--- scraper/zillow_scrape.py
+++ scraper/zillow_scrape.py
@@ -3,38 +3,48 @@ from bs4 import BeautifulSoup
 import random
 import csv
 
-# with open('proxies.txt', 'r') as f:
-#    proxies = f.read().splitlines()
+urls = [
+    'https://www.zillow.com/la-jolla-san-diego-ca/',
+    'https://www.zillow.com/la-jolla-shores-san-diego-ca/',
+    'https://www.zillow.com/la-jolla-village-san-diego-ca/',
+    'https://www.zillow.com/la-jolla-hermosa-san-diego-ca/'
+]
 
-# proxy = random.choice(proxies)
-
-header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
-          'referer':'https://www.zillow.com/homes/Missoula,-MT_rb/'}
-
-#response = requests.get('https://www.zillow.com/portland-or/', headers=header, proxies={"http": proxy})
-response = requests.get('https://www.zillow.com/la-jolla-san-diego-ca/', headers=header)
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    'referer':'https://www.zillow.com/homes/Missoula,-MT_rb/'
+}
 
 properties = []
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "html.parser")
-    search_results = soup.find(id="grid-search-results")
-    if search_results:  
-        homecards = search_results.find_all("li")
-        for card in homecards:
-            if card.find("address", {"data-test": "property-card-addr"}):
-                more_info = card.find("div", class_="property-card-data")
-                info = more_info.find_all("li")
-                data = {
-                    "address": card.find("address", {"data-test": "property-card-addr"}).text.strip(),
-                    "broker": more_info.find("div").text.strip(),
-                    "price": card.find("span", {"data-test": "property-card-price"}).text.strip(),
-                    "beds": info[0].text.strip(),
-                    "bathrooms": info[1].text.strip(),
-                    "sqft": info[2].text.strip(),
-                    "url": card.find("a", {"data-test": "property-card-link"})["href"]
-                }
-                properties.append(data)
+for url in urls:
+    try:
+        response = requests.get(url, headers=header)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            search_results = soup.find(id="grid-search-results")
+            if search_results:  
+                homecards = search_results.find_all("li")
+                for card in homecards:
+                    if card.find("address", {"data-test": "property-card-addr"}):
+                        more_info = card.find("div", class_="property-card-data")
+                        info = more_info.find_all("li")
+                        data = {
+                            "address": card.find("address", {"data-test": "property-card-addr"}).text.strip(),
+                            "broker": more_info.find("div").text.strip(),
+                            "price": card.find("span", {"data-test": "property-card-price"}).text.strip(),
+                            "beds": info[0].text.strip(),
+                            "bathrooms": info[1].text.strip(),
+                            "sqft": info[2].text.strip(),
+                            "url": card.find("a", {"data-test": "property-card-link"})["href"]
+                        }
+                        properties.append(data)
+        
+        
+    except Exception as e:
+        print(f"Error processing {url}: {str(e)}")
+        continue
 
 csv_header = ["Address", "Broker", "Price", "Beds", "Bathrooms", "Square Footage", "URL"]
 
